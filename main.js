0aa805440648459fef22df8b550581a48912164d
ChuckyNTG <11893319+ChuckyNTG@users.noreply.github.com>
2022-03-23 23:02:27 -0400

    added gravity

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	player = new Player(world._ground_coord_x, world._ground_coord_y);
 
 	//player.playerAction({ 'keycode': 3 } );
+	//keydown and keyup 
 	window.addEventListener( 'keydown', player.playerAction.bind(player) );
 	//request animation frame
 	window.requestAnimationFrame( main );
@@ -40,5 +41,10 @@ function drawWorld () {
 function drawPlayer () {
 	//draw player on ground
 	world._context.fillStyle = 'rgba(100, 0, 200, 0.5)';
+	// if player is up in the air, then gravity takes effect
+	if ( player._position_y < world._ground_coord_y )
+	{
+		player.gravity( world._ground_coord_y );
+	}
 	world._context.fillRect(player._position_x, player._position_y - player._height, player._width, player._height);
 }
--- player.js
+++ player.js
@@ -1,6 +1,8 @@
 // create player
 
 
+const VY = 3;
+const VX = 3;
 const PLAYER_HEIGHT = 25
 const PLAYER_WIDTH = 10;
 
@@ -8,6 +10,9 @@ class Player {
 	constructor (x,y) {
 		this._position_x = x;
 		this._position_y = y;
+		this._vy = VY;
+		this._vx = VX;
+		this._jumping = 0;
 		this._height = PLAYER_HEIGHT;
 		this._width = PLAYER_WIDTH;
 
@@ -49,24 +54,53 @@ class Player {
 		{
 			// TODO: model gravity
 			switch ( event.keyCode ) {
-				//up
+				//up, jump
 				case 38:
-					this._position_y -= 5;
+					this._vy = VY;
+					if ( ! this._jumping )
+					{
+						//jump
+						this._position_y -= this._vy;
+						this._jumping = 1;
+					}
 					break;
 				//down
 				case 40:
-					this._position_y += 5;
+					this._position_y += this._vy;
 					break;
 
 				//right
 				case 39:
-					this._position_x += 8;
+					this._position_x += this._vx;
 					break;
 				//left
 				case 37:
-					this._position_x -= 8;
+					this._position_x -= this._vx;
 					break;
 			}
+		}
+	}
+
+	//doesn't need to be checked everytime
+	gravity ( ground_height )
+	{
+		// if player is jumping up
+		switch ( this._jumping )
+		{
+			case 1:
+				this._vy *= .90;
+				this._position_y -= this._vy;
+				if ( this._vy < 0.10 )
+					this._jumping = 0;
+				break;
+			//else player is falling down
+			case 0:
+				this._vy *= 1.10;
+				if( ( this._position_y + this._vy ) > ground_height )
+					this._position_y = ground_height;
+				else
+					this._position_y += this._vy;
+				break;
 		}
 	}
 
